0ebf44f7a68d447f8f4980b2079232aa67794596
Zoe Dharmadi <zoedharmadi@Zoes-MacBook-Air-2.local>
2026-03-24 19:46:11 +0800

    Hide Nav Items

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,10 +1,10 @@
 ﻿export const navLinks = [
   { label: 'Dashboard', path: '/dashboard.html', icon: 'speedometer2' },
   { label: 'Manpower Management', path: '/manpower-management.html', icon: 'people' },
-  { label: 'Retention Analytics', path: '/retention-analytics.html', icon: 'graph-up' },
-  { label: 'Tutor Capacity', path: '/capacity-utilization.html', icon: 'bar-chart-line' },
-  { label: 'Marketing Insights', path: '/marketing-insights.html', icon: 'clipboard-data' },
-  { label: 'Payroll Analytics', path: '/payroll-analytics.html', icon: 'wallet' },
+  // { label: 'Retention Analytics', path: '/retention-analytics.html', icon: 'graph-up' },
+  // { label: 'Tutor Capacity', path: '/capacity-utilization.html', icon: 'bar-chart-line' },
+  // { label: 'Marketing Insights', path: '/marketing-insights.html', icon: 'clipboard-data' },
+  // { label: 'Payroll Analytics', path: '/payroll-analytics.html', icon: 'wallet' },
   { label: 'Records', path: '/records.html', icon: 'folder2-open' }
 ];
 
